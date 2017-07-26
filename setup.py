ec33f6e6128de0790140a65c6f5a7a8347f4a262
Vidushi Sharma <vidushi1000@gmail.com>
2017-07-26 10:58:04 -0400

    Added setup.py and folder

---
+++ setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name='QM_Team4',
+        version="0.1.1",
+        description='A compilation of SCF programs',
+        author='Vidushi Sharma',
+        author_email='vidushi1000@gmail.com',
+        url="https://github.com/vidsharma/QM_2017_SSS_Team4.git",
+        license='BSD-3C',
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'numpy>=1.7',
+        ],
+        extras_require={
+            'docs': [
+                'sphinx==1.2.3',  # autodoc was broken in 1.3.1
+                'sphinxcontrib-napoleon',
+                'sphinx_rtd_theme',
+                'numpydoc',
+            ],
+            'tests': [
+                'pytest',
+                'pytest-cov',
+                'pytest-pep8',
+                'tox',
+            ],
+        },
+
+        tests_require=[
+            'pytest',
+            'pytest-cov',
+            'pytest-pep8',
+            'tox',
+        ],
+
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+        ],
+        zip_safe=True,
+    )
