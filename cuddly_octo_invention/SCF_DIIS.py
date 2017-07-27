2d3773ef964ba726f647bf801505c9399f2b274d
kdbasema <kdbasema@iastate.edu>
2017-07-26 20:50:03 -0400

    Added functions for common features in separate file

--- cuddly_octo_invention/SCF_DIIS.py
+++ cuddly_octo_invention/SCF_DIIS.py
@@ -1,121 +1,89 @@
 import numpy as np
 import psi4
+import scf_func as scf
 
-# print only 4 digits
-np.set_printoptions(precision=14, suppress=True)
+def SCF_DIIS():
+    # print only 4 digits
+    np.set_printoptions(precision=14, suppress=True)
 
+    # Initialize variables for energy convergence and conditional Fock dampening
+    E_old = 0.0
 
-# Diagonalize Core H
-def diag(F, A):
-    Fp = A.T @ F @ A
-    eps, Cp = np.linalg.eigh(Fp)
-    C = A @ Cp
-    return eps, C
+    # Build a molecule
+    # psi4.geometry builds a xyz matrix from a z-matrix structure in this case
+    # physicists water molecule
+    mol = scf.mol("""
+                  O
+                  H 1 1.1
+                  H 1 1.1 2 104""")
 
-# Initialize variables for energy convergence and conditional Fock dampening
-E_old = 0.0
-F_old = None
+    nel = 5 # number of electronic orbitals (RHF)
+    e_conv = 1.e-14 # energy convergence threshold
+    d_conv = 1.e-8  # density convergence threshold
+    max_iter = 50
+    bas = scf.basis_set(mol, "sto-3g") # Build a basis
 
-# Build a molecule
-# psi4.geometry builds a xyz matrix from a z-matrix structure in this case
-# physicists water molecule
-mol = psi4.geometry("""
-O
-H 1 1.1
-H 1 1.1 2 104""")
+    mints = scf.mints_helper(bas) # Build a (MintsHelper = Object to get integrals from)
 
-nel = 5 # number of electronic orbitals (RHF)
-e_conv = 1.e-14 # energy convergence threshold
-d_conv = 1.e-8  # density convergence threshold
-damp_value = 0.20 
-damp_start = 5 
+    V = np.array(mints.ao_potential()) # Build potential
+    T = np.array(mints.ao_kinetic()) # Build kinetic
 
-bas = psi4.core.BasisSet.build(mol, target="sto-3g") # Build a basis
+    H = T + V # Core Hamiltonian
 
-mints = psi4.core.MintsHelper(bas) # Build a (MintsHelper = Object to get integrals from)
+    F = H
+    F = np.array(F)
 
-V = np.array(mints.ao_potential()) # Build potential
+    nbf = mints.nbf() # Get number of basis functions
 
-T = np.array(mints.ao_kinetic()) # Build kinetic
+    fock_list = []
+    diis_error = []
 
-H = T + V # Core Hamiltonian
+    S = np.array(mints.ao_overlap()) # Overlap matrix (S)
+    g = np.array(mints.ao_eri()) # Get AO two electron integral
 
-F = H # Initial guess for Fock matrix is core Hamiltonian
+    A = scf.A_diag(mints)
 
-F = np.array(F)
-nbf = mints.nbf() # Get number of basis functions
-
-# We want to limit the ammount of memory we call hard stop at 100 basis functions
-if (nbf > 100):
-    raise Exception("More than 100 basis functions!")
-
-fock_list = []
-diis_error = []
-
-S = np.array(mints.ao_overlap()) # Overlap matrix (S)
-
-g = np.array(mints.ao_eri()) # Get AO two electron integral
-
-# S^-1/2
-A = mints.ao_overlap()
-A.power(-0.5, 1.e-14)
-A = np.array(A)
-eps, C = diag(F, A) # Get eigenvalues eps, and eigenfunctions C
-Cocc = C[:, :nel]
-D = Cocc @ Cocc.T # Calculate density
-
-for iteration in range(40):
-
-    # Get coulomb (J) and exchange (K) matrices
-    J = np.einsum("pqrs, rs -> pq", g, D)
-    K = np.einsum("prqs, rs -> pq", g, D)
-    F = H + 2.0 * J - K # Calculate new Fock matrix
-
-    # DIIS error build
-    diis_e = np.einsum('ij, jk, kl->il', F, D, S) - np.einsum('ij, jk, kl->il', S, D, F)
-    diis_e = A.T.dot(diis_e).dot(A)
-    fock_list.append(F)
-    diis_error.append(diis_e)
-    # Build the AO gradient       
-    
-    # Build the energy
-    E_electric = np.sum((F + H) * D)
-    E_total = E_electric + mol.nuclear_repulsion_energy()
-    E_diff = E_total - E_old
-    E_old = E_total
-
-    if (iteration >= 1):
-        diis_count = len(fock_list)
-        B = np.empty((diis_count + 1, diis_count + 1))
-        B[-1, :] = -1
-        B[:, -1] = -1
-        B[-1, -1] = 0
-        for num1, e1 in enumerate(diis_error):
-            for num2, e2 in enumerate(diis_error):
-                if num2 > num1: continue
-                val = np.einsum('ij, ij->', e1, e2)
-                B[num1, num2] = val
-                B[num2, num1] = val
-
-        B[:-1, :-1] /= np.abs(B[:-1, :-1]).max()
-
-        resid = np.zeros(diis_count + 1)
-        resid[-1] = -1
-
-        ci = np.linalg.solve(B, resid)
-
-        F = np.zeros_like(F)
-        for num, c in enumerate(ci[:-1]):
-            F += c * fock_list[num]
-
-    Fp = A.T.dot(F).dot(A)
-    eps, C = diag(F, A) # Get eigenvalues eps, and eigenfunctions C
+    eps, C = scf.diag(H, A) # Get eigenvalues eps, and eigenfunctions C
     Cocc = C[:, :nel]
     D = Cocc @ Cocc.T # Calculate density
 
-    print("Iter=%3d % 16.12f  % 8.4e  "
-          % (iteration, E_total, E_diff))
-    if (abs(E_diff) < e_conv):
-        break
+    for iteration in range(1, max_iter + 1):
 
-    # Break if e_conv and d_conv are met
+        # Get coulomb (J) and exchange (K) matrices
+        J, K = scf.exc_col(g, D)
+        F = H + 2.0 * J - K # Calculate new Fock matrix
+
+        # DIIS error build
+        diis_error.append(scf.diis_e(F, D, S, A))
+    
+        fock_list.append(F)
+
+        # Build the AO gradient
+        grad = F @ D @ S - S @ D @ F
+        grad_rms = np.mean(grad ** 2) ** 0.5
+
+        # Build the energy
+        E_old, E_diff = scf.Energy(E_old, F, H, D, mol)
+
+        if (iteration >= 2):
+            diis_count = len(fock_list)
+            ci = scf.c_vec(scf.B_matrix(diis_count, diis_error), diis_count)
+            F = np.zeros_like(F)
+            F = scf.fock_matrix_diis(fock_list, ci, F)
+        
+        Fp = A.T.dot(F).dot(A)
+        eps, C = scf.diag(F, A) # Get eigenvalues eps, and eigenfunctions C
+        Cocc = C[:, :nel]
+        D = np.einsum('pi, qi->pq', Cocc, Cocc) # Calculate density
+
+        print("Iter=%3d % 16.12f  % 8.4e % 8.4e "
+              % (iteration, E_old, E_diff, grad_rms))
+    
+        if (abs(E_diff) < e_conv):
+            break
+
+        # Break if e_conv and d_conv are met
+    psi4.set_output_file("output.dat")
+    psi4.set_options({"scf_type": "pk"})
+    psi4_energy = psi4.energy("SCF/sto-3g", molecule=mol)
+    print("Energy matches Psi4 %s" % np.allclose(psi4_energy, E_old))
